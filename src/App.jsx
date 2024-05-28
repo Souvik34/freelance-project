77f02f8008ec0558e11a5cf75d95374b89b45f34
Souvik <souvik13109@gmail.com>
2024-05-28 23:38:29 +0530

    "Updated package-lock.json, added react-icons and react-scroll dependencies, modified App.jsx and index.css files"

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,9 @@ import './App.css'
 
 const App = () => {
   return (
-    <h1 className="text-3xl font-bold underline">
-    Hello world!
-  </h1>
+    <>
+    <div className='bg-slate-900'></div>
+    </>
   )
 }
 
