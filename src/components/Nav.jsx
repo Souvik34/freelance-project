44118f79048b46351bccaba08a0edb6bf0980d10
Souvik <souvik13109@gmail.com>
2024-05-30 00:45:17 +0530

    "Updated Nav component to use logo image instead of text, and changed hover text color to #4AAEF1"

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -3,6 +3,8 @@ import { useState } from "react"
 import { CiMenuFries } from "react-icons/ci";
 import { FaTimes } from "react-icons/fa";
 import { Link } from "react-scroll"
+import logo from "../assets/logo.png"
+
 
 
 const Nav = () => {
@@ -50,31 +52,34 @@ const Nav = () => {
             <div className="h-10vh flex justify-between z-50 text-white lg:py-5 px-20 py-4">
 
             <div className="flex items-center flex-1">
-                <span className="text-3xl font-bold">Logo</span>
+                <div>
+               <a href="/"><img src={logo} alt="Logo" className="logo-img w-full h-[10vh]" /></a>
+
+                </div>
             </div>
             <div className="lg:flex md:flex lg: flex-1 items center justify-end font-normal hidden">
                 <div className="flex-10">
                 <ul className="flex gap-8 mr-16 text-[18px]">
                 <Link  spy={true} smooth={true} to="Home">
-                    <li className="hover:text-fuchsia-600 transition border-b-2 border-slate-900 hover:border-fuchsia-600 cursor-pointer">Home</li>
+                    <li className="hover:text-[#4AAEF1] transition border-b-2 border-slate-900 hover:border-[#4AAEF1] cursor-pointer">Home</li>
                 </Link>
                 <Link  spy={true} smooth={true} to="Partners">
-                    <li className="hover:text-fuchsia-600 transition border-b-2 border-slate-900 hover:border-fuchsia-600 cursor-pointer">Partners</li>
+                    <li className="hover:text-[#4AAEF1] transition border-b-2 border-slate-900 hover:border-[#4AAEF1] cursor-pointer">Partners</li>
                 </Link>
                 <Link  spy={true} smooth={true} to="Services">
-                    <li className="hover:text-fuchsia-600 transition border-b-2 border-slate-900 hover:border-fuchsia-600 cursor-pointer">Services</li>
+                    <li className="hover:text-[#4AAEF1] transition border-b-2 border-slate-900 hover:border-[#4AAEF1] cursor-pointer">Services</li>
                 </Link>
                 <Link  spy={true} smooth={true} to="Industries">
-                    <li className="hover:text-fuchsia-600 transition border-b-2 border-slate-900 hover:border-fuchsia-600 cursor-pointer">Industries</li>
+                    <li className="hover:text-[#4AAEF1] transition border-b-2 border-slate-900 hover:border-[#4AAEF1] cursor-pointer">Industries</li>
                 </Link>
                 <Link  spy={true} smooth={true} to="Training">
-                    <li className="hover:text-fuchsia-600 transition border-b-2 border-slate-900 hover:border-fuchsia-600 cursor-pointer">Training</li>
+                    <li className="hover:text-[#4AAEF1] transition border-b-2 border-slate-900 hover:border-[#4AAEF1] cursor-pointer">Training</li>
                 </Link>
                 <Link  spy={true} smooth={true} to="About">
-                    <li className="hover:text-fuchsia-600 transition border-b-2 border-slate-900 hover:border-fuchsia-600 cursor-pointer">About</li>
+                    <li className="hover:text-[#4AAEF1] transition border-b-2 border-slate-900 hover:border-[#4AAEF1] cursor-pointer">About</li>
                 </Link>
                 <Link  spy={true} smooth={true} to="Contact">
-                    <li className="hover:text-fuchsia-600 transition border-b-2 border-slate-900 hover:border-fuchsia-600 cursor-pointer">Contact</li>
+                    <li className="hover:text-[#4AAEF1] transition border-b-2 border-slate-900 hover:border-[#4AAEF1] cursor-pointer">Contact</li>
                 </Link>
                 
                 </ul>
