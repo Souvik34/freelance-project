bf3631f0c70cf4b713dc6d5f08726e538d9cd6f0
Souvik <souvik13109@gmail.com>
2024-05-29 13:22:54 +0530

    "Removed Header component and updated App component to import and render Nav component"

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
 
 
 // import React from 'react'
-
+import Nav from "./components/Nav";
 const App = () => {
   return (
-    <>
-    <div className='bg-slate-900'></div>
-    </>
-  )
-}
+    
+    <div className="bg-slate-900">
+      <Nav/>
+      {/* <h2>react App</h2> */}
+    </div>
+    
+  );
+};
 
 export default App
---
+++ src/components/Nav.jsx
@@ -0,0 +1,80 @@
+
+
+
+const Nav = () => {
+    const content = <>
+        <div className="">
+
+            <ul>
+
+                <link to="Home">
+                    <li>Home</li>
+                </link>
+                <link to="Partners">
+                    <li>Partners</li>
+                </link>
+                <link to="Services">
+                    <li>Services</li>
+                </link>
+                <link to="Industries">
+                    <li>Industries</li>
+                </link>
+                <link to="Training">
+                    <li>Training</li>
+                </link>
+                <link to="About">
+                    <li>About</li>
+                </link>
+                <link to="Contact">
+                    <li>Contact</li>
+                </link>
+                
+                 
+
+
+            </ul>
+
+        </div>
+
+    </>
+    return (
+        <nav>
+            <div className="h-10vh flex justify-between z-50 text-white lg:py-5 px-20 py-4">
+
+            <div className="flex items-center flex-1">
+                <span className="text-3xl font-bold">Logo</span>
+            </div>
+            <div className="lg:flex md:flex lg: flex-1 items center justify-end font-normal hidden">
+                <div className="flex-10">
+                <ul className="flex gap-8 mr-16 text-[18px]">
+                <link to="Home">
+                    <li>Home</li>
+                </link>
+                <link to="Partners">
+                    <li>Partners</li>
+                </link>
+                <link to="Services">
+                    <li>Services</li>
+                </link>
+                <link to="Industries">
+                    <li>Industries</li>
+                </link>
+                <link to="Training">
+                    <li>Training</li>
+                </link>
+                <link to="About">
+                    <li>About</li>
+                </link>
+                <link to="Contact">
+                    <li>Contact</li>
+                </link>
+                
+                </ul>
+            </div>
+            </div>
+            </div>
+        </nav>
+    )
+}
+
+export default Nav
