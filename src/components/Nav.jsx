b5bd719ad6e03bee1e5d49e9c052fa3441034e6e
Souvik <souvik13109@gmail.com>
2024-05-29 13:49:36 +0530

    Responsiveness

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,8 +1,15 @@
 
+import { useState } from "react"
+import { CiMenuFries } from "react-icons/ci";
+import { FaTimes } from "react-icons/fa";
 import { Link } from "react-scroll"
 
 
 const Nav = () => {
+    const [click, setClick]= useState(false);
+    const handleClick = () => {
+        setClick(!click);
+    }
     const content = <>
         <div className="lg:hidden block absolute top-16 w-full left-0 right-0 bg-slate-900 transition">
 
@@ -73,6 +80,13 @@ const Nav = () => {
                 </ul>
             </div>
             </div>
+         <div>
+        {click && content}
+         </div>
+         <button className="block sm:hidden transition" onClick={handleClick}>
+            {click ? <FaTimes /> : <CiMenuFries/>}
+         </button>
+
             </div>
         </nav>
     )
