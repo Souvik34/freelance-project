4e33683d530a60dcf03b891988604e1f27bb8e3a
Souvik <souvik13109@gmail.com>
2024-05-31 23:44:43 +0530

    "Rename Nav component to Navbar, remove Home component, and update App component to use new Navbar"

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,10 @@
-
-
-// import React from 'react'
-import Nav from "./components/Nav";
+import Nav from "./components/Navbar"
 const App = () => {
   return (
     
-    <div className="bg-slate-900">
-      <Nav/>
-      {/* <h2>react App</h2> */}
-    </div>
+   <>
+   <Nav />
+   </>
     
   );
 };
