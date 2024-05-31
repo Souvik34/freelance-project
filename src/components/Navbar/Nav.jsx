ec2669c62c5c76d6a998cf7c71d757ebbbd6cf56
Souvik <souvik13109@gmail.com>
2024-06-01 00:40:50 +0530

    "Added CSS styles for Navbar component and implemented JSX structure with menu items and a 'Get Started' button"

--- src/components/Navbar/Nav.jsx
+++ src/components/Navbar/Nav.jsx
@@ -1,9 +1,36 @@
 /* eslint-disable no-unused-vars */
 import React from 'react'
 import "./Nav.css"
+import {menus} from "../../data"
+import { Link } from 'react-scroll'
+import { FaArrowRightLong } from 'react-icons/fa6'
 const Nav = () => {
   return (
-    <div>Nav</div>
+    <nav className='navbar'>
+        <div className='column hamburger'>
+        <div className='menu__group'>
+            {
+                menus.map((menu,index)=>(
+                    <Link 
+                    to={menu.routeId}
+                    className='item'
+                    activeClass='text__gradient'
+                    smooth={true}
+                    spy={true}
+                    offset={-70}
+                    key={index}>
+                    {menu.name}     
+                    </Link>
+                ))
+            }   
+        </div>
+        </div>
+        <div className='column'>
+            <Link to='about' className='btn contact__btn'>Get Started <FaArrowRightLong/></Link>
+        </div>
+        Nav
+        
+    </nav>
   )
 }
 
